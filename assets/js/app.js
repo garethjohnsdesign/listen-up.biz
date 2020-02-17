4e8d6927a33f725461d3b59c0c0f9ac561687bf0
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-02-17 13:43:19 +0000

    Update Yarn

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,9 @@
-// @codekit-prepend "/../node_modules/jquery/dist/jquery.js";
-// @codekit-prepend "/../node_modules/foundation-sites/dist/js/foundation.js";
-// @codekit-prepend "/../node_modules/motion-ui/dist/motion-ui.js";
-// @codekit-prepend "/../node_modules/what-input/dist/what-input.js";
-// @codekit-prepend "/../node_modules/aos/dist/aos.js";
-// @codekit-prepend "/../node_modules/lightgallery/dist/js/lightgallery-all.js";
+// 1. Imports
+// ----------
 
-$(document).on('turbolinks:load', function() {
+import $ from "jquery";
+import Foundation from 'foundation-sites';
+import AOS from 'aos';
 
 $(document).foundation();
 
@@ -30,30 +28,26 @@ $(function() {
 
 $(function(){
   if($('body').is('.contact')){
-    
-           var london = new mapboxgl.Map({
-                container: 'london',
-                style: 'mapbox://styles/garethjohnsdesign/cjl4u2rwc31nd2sqakytpdf6b',
-                center: [-0.138580, 51.541603],
-                zoom: 13.88,
-                scrollZoom: false
-            });
-            
-            var losangeles = new mapboxgl.Map({
-                container: 'losangeles',
-                style: 'mapbox://styles/garethjohnsdesign/ck1ja3pwy01di1cn08g5khe5a',
-                center: [-118.263695, 34.114348],
-                zoom: 10.36,
-                scrollZoom: false
-            });
-
-            var londonNav = new mapboxgl.NavigationControl();
-            london.addControl(londonNav, 'top-left');
-            
-            var losangelesNav = new mapboxgl.NavigationControl();
-            losangeles.addControl(losangelesNav, 'top-left');
+     var london = new mapboxgl.Map({
+          container: 'london',
+          style: 'mapbox://styles/garethjohnsdesign/cjl4u2rwc31nd2sqakytpdf6b',
+          center: [-0.138580, 51.541603],
+          zoom: 13.88,
+          scrollZoom: false
+      });
       
+      var losangeles = new mapboxgl.Map({
+          container: 'losangeles',
+          style: 'mapbox://styles/garethjohnsdesign/ck1ja3pwy01di1cn08g5khe5a',
+          center: [-118.263695, 34.114348],
+          zoom: 10.36,
+          scrollZoom: false
+      });
+
+      var londonNav = new mapboxgl.NavigationControl();
+      london.addControl(londonNav, 'top-left');
+      
+      var losangelesNav = new mapboxgl.NavigationControl();
+      losangeles.addControl(losangelesNav, 'top-left');
     }
   });
-
-});
