70428fa4c01ce19760c5f93e2057e3aa9bce35eb
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-02-17 20:29:05 +0000

    Initial Commit

--- assets/js/map.js
+++
@@ -1,8 +0,0 @@
-// 1. Imports
-// ----------
-import $ from "jquery";
-import mapboxgl from 'mapbox-gl';
-
-$(function() {  
-  mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FyZXRoam9obnNkZXNpZ24iLCJhIjoiY2s2cWl1a3pzMTByajNucDk3OTlzbGNsbyJ9.rPrZ8SXk_i_WVzvfDYdWcQ';
-});
