ca5af310530d2dbe488f19adb33b90c3287aeb94
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-02-17 21:12:20 +0000

    Working Script

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,4 +25,5 @@ $(function() {
 
 $(function() {  
   mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FyZXRoam9obnNkZXNpZ24iLCJhIjoiY2s2cWl1a3pzMTByajNucDk3OTlzbGNsbyJ9.rPrZ8SXk_i_WVzvfDYdWcQ';
+  window.mapboxgl = mapboxgl;
 });
